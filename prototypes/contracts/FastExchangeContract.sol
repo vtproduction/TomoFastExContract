ebcd825cb56acc7c51878a87bc9e0663f51ed26e
Lê Mạnh Niên <nienbkict@gmail.com>
2018-06-21 16:29:38 +0700

    FastExchangeContract with basic structures

---
+++ prototypes/contracts/FastExchangeContract.sol
@@ -0,0 +1,129 @@
+pragma solidity ^0.4.19;
+
+
+// ----------------------------------------------------------------------------
+// Safe maths start
+// ----------------------------------------------------------------------------
+contract SafeMath {
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+// ----------------------------------------------------------------------------
+// Safe maths end
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+// Owned contract start
+// ----------------------------------------------------------------------------
+contract Owned {
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner {
+        require(_newOwner != address(0));
+        newOwner = _newOwner;
+    }
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+}
+// ----------------------------------------------------------------------------
+// Owned contract end
+// ----------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------
+// FastExchange contract start
+// ----------------------------------------------------------------------------
+contract FastExchange is Owned, SafeMath {
+
+    struct Transaction {
+        address from;
+        uint ethValue;
+        uint estimatedTokenRate;
+        uint receiveTime;
+        uint executedTime;
+        uint status; //0: pending; 1: processing; 2: success; -1: canceled 
+    }
+
+    mapping (address => Transaction[]) transactionBook;
+
+
+    //events
+    event ReceivePendingTransaction(Transaction _transaction);
+    event UpdateTransactionStatus(Transaction _transaction);
+    //events
+
+    //modifiers
+    
+    //modifiers
+
+
+    //constructor
+    constructor() public {
+
+    }
+
+    function () public payable {
+        
+    }
+
+    function getTransactionBookCount(address _from) public returns (uint){
+        return transactionBook[_from].length;
+    }
+
+    function getLastestTransaction(address _from) public returns (Transaction) {
+        require(getTransactionBookCount(_from) > 0);
+        return (transactionBook[_from][getTransactionBookCount(_from).safeSub(1)]);
+    }
+
+    function createNewTransaction(uint _ethValue, uint _estimatedTokenRate) public {
+        uint transBookLength = transactionBook[msg.sender].length;
+        require(transBookLength == 0 
+            || transactionBook[msg.sender][transBookLength.safeSub(1)].status == 2 
+            || transactionBook[msg.sender][transBookLength.safeSub(1)].status == -1);
+        transactionBook[msg.sender].push(Transaction(msg.sender, _ethValue, _estimatedTokenRate, now, 0, 0));
+        emit ReceivePendingTransaction(transactionBook[msg.sender][transBookLength]);
+    }
+
+    function executeWhenReceiveEther(address _sender, uint _ethValue) internal {
+
+    }
+
+}
+
+
+
+
+// ----------------------------------------------------------------------------
+// FastExchange contract end
+// ----------------------------------------------------------------------------
