cb89cf15627c8767d12ed8de05a636ddf0929280
Lê Mạnh Niên <nienbkict@gmail.com>
2018-06-22 16:58:57 +0700

    re-struct contract

--- prototypes/contracts/FastExchangeContract.sol
+++ prototypes/contracts/FastExchangeContract.sol
@@ -72,11 +72,17 @@ contract FastExchange is Owned, SafeMath {
         uint estimatedTokenRate;
         uint receiveTime;
         uint executedTime;
-        uint status; //0: pending; 1: processing; 2: success; -1: canceled 
+        uint status; //0: pending; 1: processing; 2: success; -1: canceled ; -2: expried
     }
+    
+    uint public constant minEther = 0.1 ether;
+    uint public constant maxEther = 1 ether;
+    uint8 public constant maxRateChange = 5; //no bigger than 5%
+    uint public constant expriedTime = 5 * 60  * 1000; //5 minute
 
     mapping (address => Transaction[]) transactionBook;
-
+    mapping (address => uint) transactionCount;
+    mapping (address => Transaction) lastestTransaction;
 
     //events
     event ReceivePendingTransaction(Transaction _transaction);
@@ -84,7 +90,16 @@ contract FastExchange is Owned, SafeMath {
     //events
 
     //modifiers
-    
+    modifier AccepableEther(uint _etherValue) {
+        require(_etherValue >= minEther);
+        require(_etherValue <= maxEther);
+        _;
+    } 
+
+    modifier TransactionNotExpried(uint _transactionTime) {
+        require(safeAdd(_transactionTime,expriedTime) < now);
+        _;
+    }
     //modifiers
 
 
@@ -94,29 +109,42 @@ contract FastExchange is Owned, SafeMath {
     }
 
     function () public payable {
-        
+        executeWhenReceiveEther(msg.sender, msg.value);
     }
 
     function getTransactionBookCount(address _from) public returns (uint){
         return transactionBook[_from].length;
     }
 
-    function getLastestTransaction(address _from) public returns (Transaction) {
+    function getLastestTransaction(address _from) public returns (address, uint, uint, uint, uint, uint) {
         require(getTransactionBookCount(_from) > 0);
-        return (transactionBook[_from][getTransactionBookCount(_from).safeSub(1)]);
+        Transaction storage t = safeSub(transactionBook[_from][getTransactionBookCount(_from)],1);
+        return (t.from, t.ethValue, t.estimatedTokenRate, t.receiveTime, t.executedTime, t.status);
+    }
+
+    function _changeLastestTransactionStatus(address _from, uint _newStatus) private {
+        Transaction storage transaction = getLastestTransaction(_from);
+        transaction.status = _newStatus;
+        emit UpdateTransactionStatus(transaction);
     }
 
     function createNewTransaction(uint _ethValue, uint _estimatedTokenRate) public {
         uint transBookLength = transactionBook[msg.sender].length;
         require(transBookLength == 0 
-            || transactionBook[msg.sender][transBookLength.safeSub(1)].status == 2 
-            || transactionBook[msg.sender][transBookLength.safeSub(1)].status == -1);
+            || transactionBook[msg.sender][safeSub(transBookLength,1)].status == 2 
+            || transactionBook[msg.sender][safeSub(transBookLength,1)].status == -1);
         transactionBook[msg.sender].push(Transaction(msg.sender, _ethValue, _estimatedTokenRate, now, 0, 0));
         emit ReceivePendingTransaction(transactionBook[msg.sender][transBookLength]);
     }
 
-    function executeWhenReceiveEther(address _sender, uint _ethValue) internal {
-
+    function executeWhenReceiveEther(address _sender, uint _ethValue) internal AccepableEther(_ethValue){
+        Transaction storage transaction
+            = getLastestTransaction(_sender);
+        if(safeAdd(transaction.receiveTime,expriedTime) <= now){
+            _changeLastestTransactionStatus(_sender, -2);
+            _sender.send(_ethValue);
+            return;
+        }
     }
 
 }
